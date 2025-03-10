0a2255475cf0e6ef7b2adf4d01994d937afb9110
venusai-mabbu <venusai.mabbu.2003@gmail.com>
2025-03-10 16:49:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('dotenv').config();
+// console.log(process.env.JWT_SECRET);
+const mongoose = require('mongoose');
+const cors = require('cors');
+const webRoutes = require('./routes/webRoutes');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+
+//   uncomment later
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+  credentials: true
+}));
+
+
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.json());
+
+
+
+
+
+
+
+const dbURI = process.env.DB_URL;
+console.log(dbURI)
+mongoose.connect(dbURI)
+  .then((result) => {app.listen(5000);
+                     console.log("DB connected");
+  })
+  .catch((err) => console.log(err));
+
+// app.get('*', checkUser);
+
+app.use(webRoutes);
---
+++ models/Post.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const commentSchema = new Schema({
+    user: { type: String }, 
+    comment: { type: String }, 
+    createdAt: { type: Date, default: Date.now },
+    booked: {
+        type: [String], 
+    },
+    comUpvote: { type: Number,default:0 },
+    comDownvote: { type: Number,default:0 }, 
+    upvoters: {
+        type: [String], 
+    },
+    downvoters: {
+        type: [String], 
+    },
+});
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Please enter title'],
+    },
+    description: {
+        type: String,
+        required: [true, 'This field cannot be empty'],
+    },
+    category: String,
+    subcategory: String,
+    section: String,
+    upvote: {
+        type:Number,
+        default:0
+    },
+    downvote: {
+        type:Number,
+        default:0
+    },  
+    likes: {
+        type:Number,
+        default:0
+    },
+    upvotes: {
+        type: [String], 
+    },
+    downvotes: {
+        type: [String], 
+    },
+    loves: {
+        type: [String], 
+    },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    comments: [commentSchema]
+}, 
+{
+    timestamps: true
+});
+
+const PostModel = model('Post', postSchema);
+
+module.exports = PostModel;
